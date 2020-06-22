5f7c7ab249960cd0ccb8e20456afa86f61e277c6
sampa17 <sampa@DESKTOP-8SP922E.home>
2020-06-22 19:11:23 +0200

    bloc de test

---
+++ src/main/java/net/mcreator/project/block/TestblocBlock.java
@@ -0,0 +1,51 @@
+
+package net.mcreator.project.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.state.properties.SlabType;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.SlabBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.project.ProjectModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@ProjectModElements.ModElement.Tag
+public class TestblocBlock extends ProjectModElements.ModElement {
+	@ObjectHolder("project:testbloc")
+	public static final Block block = null;
+	public TestblocBlock(ProjectModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends SlabBlock {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.STONE).hardnessAndResistance(1f, 10f).lightValue(0));
+			setRegistryName("testbloc");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, state.get(TYPE) == SlabType.DOUBLE ? 2 : 1));
+		}
+	}
+}
