ccbb3d9603d3f7d647dbf06ea5560de45090ebfc
Darkcp <darkc@192.168.1.50>
2020-06-30 10:10:15 +0200

    test

---
+++ src/main/java/net/mcreator/project/block/RfgbsxBlock.java
@@ -0,0 +1,48 @@
+
+package net.mcreator.project.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.project.itemgroup.TabmodItemGroup;
+import net.mcreator.project.ProjectModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@ProjectModElements.ModElement.Tag
+public class RfgbsxBlock extends ProjectModElements.ModElement {
+	@ObjectHolder("project:rfgbsx")
+	public static final Block block = null;
+	public RfgbsxBlock(ProjectModElements instance) {
+		super(instance, 39);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(TabmodItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).lightValue(0));
+			setRegistryName("rfgbsx");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
