d2cec5f615e00ff9e81aeabab582f1c59772e918
sampa17 <sampa@DESKTOP-8SP922E.home>
2020-06-30 11:51:34 +0200

    update 1.6 bug fix

--- src/main/java/net/mcreator/project/block/TitanebarrierBlock.java
+++ src/main/java/net/mcreator/project/block/TitanebarrierBlock.java
@@ -12,14 +12,19 @@ import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Rotation;
 import net.minecraft.util.Direction;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.DirectionProperty;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.BlockItem;
 import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.SoundType;
+import net.minecraft.block.DirectionalBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Block;
 
@@ -49,8 +54,10 @@ public class TitanebarrierBlock extends ProjectModElements.ModElement {
 		RenderTypeLookup.setRenderLayer(block, RenderType.getTranslucent());
 	}
 	public static class CustomBlock extends Block {
+		public static final DirectionProperty FACING = DirectionalBlock.FACING;
 		public CustomBlock() {
 			super(Block.Properties.create(Material.IRON).sound(SoundType.METAL).hardnessAndResistance(1f, 10f).lightValue(0).notSolid());
+			this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.SOUTH));
 			setRegistryName("titanebarrier");
 		}
 
@@ -71,7 +78,47 @@ public class TitanebarrierBlock extends ProjectModElements.ModElement {
 
 		@Override
 		public VoxelShape getShape(BlockState state, IBlockReader world, BlockPos pos, ISelectionContext context) {
-			return VoxelShapes.create(0D, 0D, 0D, 0.1D, 1D, 1D);
+			switch ((Direction) state.get(FACING)) {
+				case SOUTH :
+				case NORTH :
+				default :
+					return VoxelShapes.create(0D, 0D, 0D, 1D, 1D, 0.1D);
+				case EAST :
+				case WEST :
+					return VoxelShapes.create(0D, 1D, 0D, 1D, 0.9D, 1D);
+				case UP :
+				case DOWN :
+					return VoxelShapes.create(0D, 1D, 1D, 1D, 0D, 0.9D);
+			}
+		}
+
+		@Override
+		protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+			builder.add(FACING);
+		}
+
+		@Override
+		public BlockState rotate(BlockState state, Rotation rot) {
+			if (rot == Rotation.CLOCKWISE_90 || rot == Rotation.COUNTERCLOCKWISE_90) {
+				if ((Direction) state.get(FACING) == Direction.WEST || (Direction) state.get(FACING) == Direction.EAST) {
+					return state.with(FACING, Direction.UP);
+				} else if ((Direction) state.get(FACING) == Direction.UP || (Direction) state.get(FACING) == Direction.DOWN) {
+					return state.with(FACING, Direction.WEST);
+				}
+			}
+			return state;
+		}
+
+		@Override
+		public BlockState getStateForPlacement(BlockItemUseContext context) {
+			Direction facing = context.getFace();
+			if (facing == Direction.WEST || facing == Direction.EAST)
+				facing = Direction.UP;
+			else if (facing == Direction.NORTH || facing == Direction.SOUTH)
+				facing = Direction.EAST;
+			else
+				facing = Direction.SOUTH;
+			return this.getDefaultState().with(FACING, facing);
 		}
 
 		@Override
